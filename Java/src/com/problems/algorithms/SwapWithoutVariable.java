890d6098e99318049190f26a5d116c53c74e98c5
Rajan Bhandari <rbhand4@uic.edu>
2016-02-08 23:45:11 -0600

    First Test

---
+++ Java/src/com/problems/algorithms/SwapWithoutVariable.java
@@ -0,0 +1,14 @@
+package com.problems.algorithms;
+
+public class SwapWithoutVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+
+	}
+
+}
