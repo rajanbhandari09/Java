c63f9fd231cf88f4cee6d663c23e7f3c6c4f1bc7
rajanbhandari09 <rbhand4@uic.edu>
2016-07-01 16:59:02 -0500

    Prime Number detection algorithm added

---
+++ Hackerrank/src/com/problems/algorithms/PrimeNumber.java
@@ -0,0 +1,42 @@
+/*Notes:
+1. 1 is neither prime nor composite number.
+2. Below is a solution for calculating square root of a number with O(sqrt(n)) time complexity.
+*/
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int testcases = sc.nextInt();
+		for(int i=0;i<testcases;i++){
+			int input = sc.nextInt();
+			checkPrime(input);
+
+
+
+
+		}
+	}
+
+	public static void checkPrime(int input){
+		if(input==1)
+			System.out.println("Not prime");
+		else{
+			for(int i=2;i*i<=input;i++){
+				if(input%i==0){
+					System.out.println("Not prime");
+					return;
+				}
+
+
+			}
+
+			System.out.println("Prime");
+		}    
+
+	}
+
+
+
+
+
+}
